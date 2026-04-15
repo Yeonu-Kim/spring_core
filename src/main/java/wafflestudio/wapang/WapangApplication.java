e28d3cf34dd5edd8d91ca623558c2738bd719bdd
Yeonu-Kim <ywk0524@snu.ac.kr>
2026-04-15 21:31:01 +0900

    initial commit

---
+++ src/main/java/wafflestudio/wapang/WapangApplication.java
@@ -0,0 +1,13 @@
+package wafflestudio.wapang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WapangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WapangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/wafflestudio/wapang/WapangApplicationTests.java
@@ -0,0 +1,13 @@
+package wafflestudio.wapang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WapangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
